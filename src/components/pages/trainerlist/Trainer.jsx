55a7f18be4e53c4fa844ea2e582dcb845edbe824
VishvaViz <vishvabulls@gmail.com>
2024-06-25 15:22:22 +0530

    updated

--- src/components/pages/trainerlist/Trainer.jsx
+++ src/components/pages/trainerlist/Trainer.jsx
@@ -11,21 +11,42 @@ const Trainers = ({ trainerFilteredData }) => {
   const baseUrl = process.env.REACT_APP_API_URL;
   console.log("baseUrl", baseUrl)
   // const navigate = useNavigate();
-  // const location = useLocation();
+  const location = useLocation();
   const [selecteduser, setSelecteduser] = useState(null);
   const [hoveredIndex, setHoveredIndex] = useState(null);
   const [trainerDetails, setTrainerDetails] = useState([])
 
+  // useEffect(() => {
+  //   Axios.get(`${baseUrl}/trainer/getAllTrainerDetails`)
+  //     .then((resp) => {
+  //       setTrainerDetails(resp.data?.trainerDetails)
+
+      
+  //     })
+  //     .catch((error) => {
+  //       console.log(error)
+  //     })
+  // }, [])
+
   useEffect(() => {
-    Axios.get(`${baseUrl}/trainer/getAllTrainerDetails`)
-      .then((resp) => {
-        setTrainerDetails(resp.data?.trainerDetails)
-        console.log('trainerDetails',resp.data?.trainerDetails)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }, [])
+    function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+      }
+      return array;
+    }
+
+    if (trainerFilteredData?.length > 0) {
+      setTrainerDetails(shuffleArray(trainerFilteredData))
+    }
+    else{
+      setTrainerDetails(shuffleArray(trainerFilteredData))
+    }
+
+
+  }, [location.pathname,trainerFilteredData])
+  
 
 
 
@@ -60,10 +81,10 @@ const Trainers = ({ trainerFilteredData }) => {
         className="trainers-list"
       >
         {
-          trainerFilteredData?.length > 0 ?
+          trainerDetails?.length > 0 ?
             <>
 
-              {trainerFilteredData?.map((trainer, index) => {
+              {trainerDetails?.map((trainer, index) => {
                 // console.log('trainer',trainer)
                 return (
 
@@ -145,7 +166,7 @@ const Trainers = ({ trainerFilteredData }) => {
                                           image ?
                                             <img className="w-[30px] h-[30px] " src={image} alt="" />
                                             :
-                    
+
                                             <div className="flex justify-center items-center w-[30px] h-[30px] bg-slate-400 rounded-full">
                                               <span className=" capitalize text-black text-sm">{name[0]}</span>
                                             </div>
@@ -198,7 +219,7 @@ const Trainers = ({ trainerFilteredData }) => {
                                 </div>
                                 <div className="card-back-content">
                                   <h3 className="card-back-name">
-                                    { trainer?.fullName?.split(' ')?.[0]}
+                                    {trainer?.fullName?.split(' ')?.[0]}
                                   </h3>
                                   <p className="card-back-domain "
                                     style={{
@@ -254,14 +275,14 @@ const Trainers = ({ trainerFilteredData }) => {
 
                                     <div key={index} className="slider-container-child">
                                       {
-                                        val.image?
-                                        <img src={val.image} alt="" className="w-5 h-5" />
-                                        :
-                                        <div className="flex justify-center items-center w-5 h-5 bg-slate-300 rounded-full">
-                                          <span className=" capitalize text-black text-sm">
+                                        val.image ?
+                                          <img src={val.image} alt="" className="w-5 h-5" />
+                                          :
+                                          <div className="flex justify-center items-center w-5 h-5 bg-slate-300 rounded-full">
+                                            <span className=" capitalize text-black text-sm">
                                               {val?.name[0]}
-                                          </span>
-                                        </div>
+                                            </span>
+                                          </div>
                                       }
                                       <input
                                         className="relative rounded-[5px]]"
