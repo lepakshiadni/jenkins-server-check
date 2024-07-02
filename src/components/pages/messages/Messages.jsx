2c4edc87f475e3e3e75f0ce958591cdc9f413454
tiwarirakshit02 <Rakshit.t@mindstaytechnology.in>
2024-07-02 11:02:12 +0530

    Update Messages.jsx
    Signed-off-by: tiwarirakshit02 <Rakshit.t@mindstaytechnology.in>

--- src/components/pages/messages/Messages.jsx
+++ src/components/pages/messages/Messages.jsx
@@ -1,17 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import Time from 'timesago'
-import { useSelector } from "react-redux";
-
+import React, { useEffect, useState } from 'react';
+import Time from 'timesago';
+import { useSelector } from 'react-redux';
 
 function Messages({ messages, own, selecteduser }) {
-  const [ownUser, setOwnUser] = useState(null)
+  const [ownUser, setOwnUser] = useState(null);
 
-  const employer = useSelector(({ employerSignUp }) => {
-    return employerSignUp?.employerDetails
-  })
-  const trainer = useSelector(({ trainerSignUp }) => {
-    return trainerSignUp?.trainerDetails;
-  })
+  const employer = useSelector(({ employerSignUp }) => employerSignUp?.employerDetails);
+  const trainer = useSelector(({ trainerSignUp }) => trainerSignUp?.trainerDetails);
 
   useEffect(() => {
     if (employer?.success) {
@@ -21,84 +16,64 @@ function Messages({ messages, own, selecteduser }) {
       setOwnUser(trainer?.trainerDetails);
     }
   }, [employer, trainer]);
-  const timeagp = Time(messages.createdAt)
+
+  const timeAgo = Time(messages?.createdAt);
+
   return (
-    <div >
-      {
-        own ?
-
-          <div className="flex flex-row-reverse gap-4 mr-[21px] space-y-1 mb-[20px] items-center ">
-            {/* own message */}
-
-            <div className="">
-              {
-                ownUser?.basicInfo.profileImg ? <><img
-                  className="Ellipse22 w-[40px] h-[40px] rounded-full "
-                  src={ownUser?.basicInfo.profileImg}
-                  alt=""
-                /></> : <>
-                  <div className='w-[40px] h-[40px] rounded-full flex justify-center items-center bg-slate-500'>
-                    <p className="text-['Poppins'] text-lg"> {ownUser?.fullName[0]}</p>
-                  </div>
-                </>
-              }
-
-            </div>
-
-            <div className="w-auto">
-                <div className=" text-[#fff]  text-xs font-normal font-['Poppins'] pl-[10px]  bg-[#2676c2] text-end rounded-tl-lg rounded-tr-[20px] rounded-bl-lg flex justify-end pr-[10px] h-9 items-center ">
-                  {messages?.text}
-                </div>
-              <div>
-                <div className="messageBottom text-zinc-400 text-end mt-[5px]  text-[10px] font-normal font-['Poppins'] ">{timeagp}</div>
+    <div>
+      {own ? (
+        <div className="flex flex-row-reverse gap-4 mr-[21px] space-y-1 mb-[20px] items-center">
+          <div>
+            {ownUser?.basicInfo?.profileImg ? (
+              <img
+                className="w-[40px] h-[40px] rounded-full"
+                src={ownUser?.basicInfo?.profileImg}
+                alt=""
+              />
+            ) : (
+              <div className='w-[40px] h-[40px] rounded-full flex justify-center items-center bg-slate-500'>
+                <p className="text-lg">{ownUser?.fullName[0]}</p>
               </div>
-            </div>
-
+            )}
           </div>
-
-
-          :
-
-          <div className="flex ml-[21px] space-x-1 mb-[20px] ">
-
-            {/* friednmessge */}
-
-            <div className="">
-              {
-                selecteduser?.basicInfo?.profileImg ? <>
-                  <img
-                className="Ellipse22 w-[40px] h-[40px] rounded-full mt-[10px]"
+          <div className="w-auto">
+            <div className="text-white text-xs font-normal bg-[#2676c2] text-end rounded-tl-lg rounded-tr-[20px] rounded-bl-lg flex justify-end pr-[10px] h-9 items-center">
+              {messages?.text}
+            </div>
+            <div className="messageBottom text-zinc-400 text-end mt-[5px] text-[10px]">
+              {timeAgo}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="flex ml-[21px] space-x-1 mb-[20px]">
+          <div>
+            {selecteduser?.basicInfo?.profileImg ? (
+              <img
+                className="w-[40px] h-[40px] rounded-full mt-[10px]"
                 src={selecteduser?.basicInfo?.profileImg}
                 alt=""
               />
-                </> : <>
-                  <div className='w-[40px] h-[40px] rounded-full mt-[10px] flex justify-center items-center bg-slate-500'>
-                    <p className="text-['Poppins'] text-lg"> {selecteduser?.fullName[0]}</p>
-                  </div>
-                </>
-              }
+            ) : (
+              <div className='w-[40px] h-[40px] rounded-full mt-[10px] flex justify-center items-center bg-slate-500'>
+                <p className="text-lg">{selecteduser?.fullName[0]}</p>
+              </div>
+            )}
+          </div>
+          <div>
+            <div className="w-full h-9 bg-zinc-100 rounded-tl-[20px] rounded-tr-lg rounded-br-lg border border-gray-200 flex items-center px-2">
+              <div className="text-neutral-500 text-xs font-normal">
+                {messages?.text}
+              </div>
             </div>
-            <div>
-              <div className="Rectangle128 m-[10px] w-[100%] flex h-9  bg-zinc-100 rounded-tl-[20px] rounded-tr-lg rounded-br-lg border border-gray-200" >
-                <div className=" text-neutral-500 text-xs font-normal font-['Poppins'] m-[10px] text-start">
-                  {messages?.text}
-                </div>
-              </div>
-              <div>
-                <div className="messageBottom text-zinc-400 text-[10px] font-normal font-['Poppins'] ml-[10px]">{timeagp}</div>
-              </div>
+            <div className="messageBottom text-zinc-400 text-[10px] ml-2">
+              {timeAgo}
             </div>
           </div>
-
-
-      }
-
-
-
-
-
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Messages
+export default Messages;
