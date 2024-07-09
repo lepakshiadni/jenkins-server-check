8c2129273a3a2c023324c402d0ad0b32311929df
tiwarirakshit02 <Rakshit.t@mindstaytechnology.in>
2024-07-09 17:13:45 +0530

    Update Messages.jsx
    Signed-off-by: tiwarirakshit02 <Rakshit.t@mindstaytechnology.in>

--- src/components/pages/messages/Messages.jsx
+++ src/components/pages/messages/Messages.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
 import Time from "timesago";
 import { useSelector } from "react-redux";
-
-function Messages({ messages, own, selecteduser, typingStatus }) {
+import axios from "axios";
+const baseUrl = process.env.REACT_APP_API_URL;
+function Messages({unreadMsgValue, messages, own, selecteduser, typingStatus }) {
   const [ownUser, setOwnUser] = useState(null);
   const [timeAgo, setTimeAgo] = useState(Time(messages?.createdAt));
-  console.log(typingStatus, "typingStatus");
+  // // console.log(messages, "messages");
+  const markAsReadTimeoutRef = React.useRef(null);
   const employer = useSelector(
     ({ employerSignUp }) => employerSignUp?.employerDetails
   );
   const trainer = useSelector(
     ({ trainerSignUp }) => trainerSignUp?.trainerDetails
   );
+
 
   useEffect(() => {
     if (employer?.success) {
@@ -29,25 +32,69 @@ function Messages({ messages, own, selecteduser, typingStatus }) {
     return () => clearInterval(intervalId); // Cleanup interval on component unmount
   }, [messages?.createdAt]);
 
-  // useEffect(() => {
-  //   if (!own && !messages.isRead) {
-  //     updateMessageStatus(messages._id);
-  //   }
-  // }, [messages, own]);
+  useEffect(() => {
+    if (!own && !messages.isRead) {
+      updateMessageStatus(messages._id);
+    }
+  }, [messages, own]);
 
-  // const updateMessageStatus = async (messageId) => {
-  //   try {
-  //     const response = await axios.put(`/api/updateMessageStatus/${messageId}`);
-  //     if (response.data.success) {
-  //       console.log('Message status updated successfully', response.data.updatedMessage);
-  //     } else {
-  //       console.error('Failed to update message status');
-  //     }
-  //   } catch (error) {
-  //     console.error('Error updating message status:', error);
-  //   }
-  // };
+  const updateMessageStatus = async (messageId) => {
+    // console.log(messageId, 'messageId')
+    try {
+      const response = await axios.put(`${baseUrl}/message/updateMessageStatus/${messageId}`);
+      if (response.data.success) {
+        // console.log('Message status updated successfully', response.data.updatedMessage);
+      } else {
+        console.error('Failed to update message status');
+      }
+    } catch (error) {
+      console.error('Error updating message status:', error);
+    }
+  };
 
+  // // console.log(messages.conversationId,"messages")
+  useEffect(() => {
+    if (messages.conversationId && !messages.isRead) {
+      if(markAsReadTimeoutRef.current){
+        clearTimeout(markAsReadTimeoutRef.current);
+      }
+      markAsReadTimeoutRef.current = setTimeout(() => {
+        markMessageAsRead(messages.conversationId);
+      }, 2000);
+
+      return () => {
+        if(markAsReadTimeoutRef.current){
+          clearTimeout(markAsReadTimeoutRef.current);
+        }
+      };
+    }
+  }, [messages]);
+
+  const markMessageAsRead = async (conversationId) => {
+    // console.log(conversationId, 'conversationId');
+    if(!conversationId) {
+        console.error('Invalid conversation ID:', conversationId);
+        return;
+    }
+    try {
+        const response = await axios.put(
+            `${baseUrl}/conversation/markAsRead/${conversationId}`,
+            {},
+        );
+  
+        if (response.status === 200) {
+            // console.log('Message marked as read:', response.data);
+            unreadMsgValue(true);
+            // Emit a socket event to notify other users
+            // socket.current.emit("readMessage", { conversationId, userId: user?._id });
+        } else {
+            console.error('Failed to mark message as read:', response.data.message);
+        }
+    } catch (error) {
+        console.error('Error marking message as read:', error);
+    }
+  };
+  
   return (
     <div>
       {own ? (
@@ -98,6 +145,7 @@ function Messages({ messages, own, selecteduser, typingStatus }) {
             <div className="messageBottom text-zinc-400 text-[10px] ml-2">
               {timeAgo}
             </div>
+            {/* <div>{typingStatus == selecteduser._id ? "typing...." : null}</div> */}
           </div>
           {/* <div>{typingStatus == selecteduser._id ? "typing...." : null}</div> */}
         </div>
